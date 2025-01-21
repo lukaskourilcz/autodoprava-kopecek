aec12b5c39550615197c7cca97da681a8d08542f
Lukas Kouril <kouril.lukas@gmail.com>
2025-01-21 15:48:09 +0700

    services grid wip

--- src/app/sluzby/page.tsx
+++ src/app/sluzby/page.tsx
@@ -45,42 +45,42 @@ export default function Sluzby() {
 
   const irregularTransportSections = [
     {
-      icon: <Backpack className="w-20 h-20 text-gray-600" />,
+      icon: <Backpack className="w-6 h-6 text-gray-600" />,
       title: t("services.irregularBusTransport.schoolTransport.title"),
       description: t(
         "services.irregularBusTransport.schoolTransport.description"
       ),
     },
     {
-      icon: <Volleyball className="w-20 h-20 text-gray-600" />,
+      icon: <Volleyball className="w-6 h-6 text-gray-600" />,
       title: t("services.irregularBusTransport.sportsTransport.title"),
       description: t(
         "services.irregularBusTransport.sportsTransport.description"
       ),
     },
     {
-      icon: <Building2 className="w-20 h-20 text-gray-600" />,
+      icon: <Building2 className="w-12 h-12 text-gray-600" />,
       title: t("services.irregularBusTransport.groupsTransport.title"),
       description: t(
         "services.irregularBusTransport.groupsTransport.description"
       ),
     },
     {
-      icon: <PlaneTakeoff className="w-20 h-20 text-gray-600" />,
+      icon: <PlaneTakeoff className="w-6 h-6 text-gray-600" />,
       title: t("services.irregularBusTransport.airportTransfers.title"),
       description: t(
         "services.irregularBusTransport.airportTransfers.description"
       ),
     },
     {
-      icon: <BookHeart className="w-20 h-20 text-gray-600" />,
+      icon: <BookHeart className="w-6 h-6 text-gray-600" />,
       title: t("services.irregularBusTransport.weddingTransport.title"),
       description: t(
         "services.irregularBusTransport.weddingTransport.description"
       ),
     },
     {
-      icon: <Bus className="w-20 h-20 text-gray-600" />,
+      icon: <Bus className="w-6 h-6 text-gray-600" />,
       title: t("services.irregularBusTransport.replacementTransport.title"),
       description: t(
         "services.irregularBusTransport.replacementTransport.description"
@@ -111,70 +111,48 @@ export default function Sluzby() {
             </div>
           ))}
         </div>
-{/* Nepravidelná autobusová doprava */}
-<div className="mb-12">
-  <h3 className="text-3xl font-bold text-gray-800 mb-6">
+        <div className="mb-12">
+  <h3 className="text-4xl font-bold text-gray-800 mb-12 text-center">
     {t("services.irregularBusTransport.title")}
   </h3>
-  <div className="space-y-12">
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-8 gap-y-12">
     {irregularTransportSections.map((section, index) => (
       <div
         key={index}
-        className={`flex flex-col md:flex-row ${
-          index % 2 === 0 ? "md:flex-row-reverse" : ""
-        } items-center`}
+        className="flex flex-col items-center text-center bg-white shadow-md rounded-lg p-6"
       >
-        <div className="md:w-1/3 flex justify-center mb-6 md:mb-0">
-          <div className="flex items-center justify-center">
-            {React.cloneElement(section.icon, {
-              className: "w-16 h-16 text-gray-600", // Resized icons
-            })}
-          </div>
-        </div>
-        <div className="md:w-2/3 text-center md:text-left">
-          <h4 className="text-2xl font-semibold text-gray-800 mb-2">
-            {section.title}
-          </h4>
-          <p className="text-gray-600">{section.description}</p>
-        </div>
+        <div className="w-12 h-12 mb-4 text-gray-600">{section.icon}</div>
+        <h4 className="text-xl font-semibold text-gray-800 mb-2">
+          {section.title}
+        </h4>
+        <p className="text-gray-600 text-sm">{section.description}</p>
       </div>
     ))}
-  </div>
-</div>
-
-{/* Firemní linková doprava */}
-<div className="mb-12">
-  <h3 className="text-3xl font-bold text-gray-800 mb-6">
-    {t("services.corporateTransport.title")}
-  </h3>
-  <div className="flex flex-col md:flex-row-reverse items-center">
-    <div className="md:w-1/3 flex justify-center mb-6 md:mb-0">
-      <Route className="w-16 h-16 text-gray-600" />
-    </div>
-    <div className="md:w-2/3 text-center md:text-left">
-      <p className="text-gray-600">
+    <div className="flex flex-col items-center text-center bg-white shadow-md rounded-lg p-6">
+      <div className="w-12 h-12 mb-4 text-gray-600">
+        <Route className="w-full h-full" />
+      </div>
+      <h4 className="text-xl font-semibold text-gray-800 mb-2">
+        {t("services.corporateTransport.title")}
+      </h4>
+      <p className="text-gray-600 text-sm">
         {t("services.corporateTransport.description")}
       </p>
     </div>
-  </div>
-</div>
-
-{/* Nákladní doprava */}
-<div>
-  <h3 className="text-3xl font-bold text-gray-800 mb-6">
-    {t("services.cargoTransport.title")}
-  </h3>
-  <div className="flex flex-col md:flex-row items-center">
-    <div className="md:w-1/3 flex justify-center mb-6 md:mb-0">
-      <Truck className="w-16 h-16 text-gray-600" />
-    </div>
-    <div className="md:w-2/3 text-center md:text-left">
-      <p className="text-gray-600">
+    <div className="flex flex-col items-center text-center bg-white shadow-md rounded-lg p-6">
+      <div className="w-12 h-12 mb-4 text-gray-600">
+        <Truck className="w-full h-full" />
+      </div>
+      <h4 className="text-xl font-semibold text-gray-800 mb-2">
+        {t("services.cargoTransport.title")}
+      </h4>
+      <p className="text-gray-600 text-sm">
         {t("services.cargoTransport.description")}
       </p>
     </div>
   </div>
 </div>
+
 
 
       </div>
