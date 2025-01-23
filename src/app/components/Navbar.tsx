06dba8717d24f412388caf162eb12b98f12a560a
Lukas Kouril <kouril.lukas@gmail.com>
2025-01-23 16:42:04 +0700

    header fixed

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -40,118 +40,119 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 bg-gray-800 opacity-90 text-white p-4 flex justify-between items-center z-50">
-      {/* Logo and Brand */}
-      <div className="flex items-center space-x-4">
-        <div className="logo-container flex items-center">
-          <Image
-            src="/pics/logo-white-nav.png"
-            alt="Logo"
-            width={85}
-            height={85}
-            className="logo-animation mx-3"
-            priority
-          />
-          <Link
-            href={`/?lang=${activeLocale}#home`}
-            className="hover:text-gray-200 block ml-4"
-          >
-            <span className="font-bold text-lg uppercase leading-tight">
-              <span className="text-yellow-400">AUTODOPRAVA</span>
-              <br />
-              <span>KOPEČEK.CZ</span>
-            </span>
-          </Link>
-        </div>
-        {/* Hamburger Icon */}
-        <button
-          className="sm:block lg:hidden text-white focus:outline-none"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          {menuOpen ? <X size={28} /> : <Menu size={28} />}
-        </button>
-      </div>
-
-      {/* Navigation Links */}
-      <ul
-        className={`${
-          menuOpen ? "block" : "hidden"
-        } lg:flex space-y-4 lg:space-y-0 lg:space-x-6 bg-gray-800 lg:bg-transparent lg:flex-row lg:static absolute top-full left-0 w-full lg:w-auto p-4 pl-8 lg:p-0`}
+  {/* Logo and Brand */}
+  <div className="flex items-center space-x-4">
+    <div className="logo-container flex items-center">
+      <Image
+        src="/pics/logo-white-nav.png"
+        alt="Logo"
+        width={85}
+        height={85}
+        className="logo-animation mx-3"
+        priority
+      />
+      <Link
+        href={`/?lang=${activeLocale}#home`}
+        className="hover:text-gray-200 block ml-4"
       >
-        <li>
-          <Link
-            href={`/?lang=${activeLocale}#about`}
-            className="hover:text-yellow-400 block"
-          >
-            {t("about.title")}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`/?lang=${activeLocale}#services`}
-            className="hover:text-yellow-400 block"
-          >
-            {t("services.title")}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`/?lang=${activeLocale}#fleet`}
-            className="hover:text-yellow-400 block"
-          >
-            {t("fleet.title")}
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={`/?lang=${activeLocale}#contact`}
-            className="hover:text-yellow-400 block"
-          >
-            {t("contact.title")}
-          </Link>
-        </li>
-      </ul>
+        <span className="font-bold text-lg uppercase leading-tight">
+          <span className="text-yellow-400">AUTODOPRAVA</span>
+          <br />
+          <span>KOPEČEK.CZ</span>
+        </span>
+      </Link>
+    </div>
+    {/* Hamburger Icon */}
+    <button
+      className="sm:block lg:hidden text-white focus:outline-none"
+      onClick={() => setMenuOpen(!menuOpen)}
+    >
+      {menuOpen ? <X size={28} /> : <Menu size={28} />}
+    </button>
+  </div>
 
-      {/* Language Selector */}
-      <div className="relative">
-        <button
-          onClick={() => setDropdownVisible(!dropdownVisible)}
-          className="flex items-center space-x-2 bg-gray-700 px-4 py-2 rounded-md hover:bg-gray-600"
-        >
-          <Image
-            src={
-              languages.find((lang) => lang.code === activeLocale)?.flag || ""
-            }
-            alt={activeLocale}
-            width={20}
-            height={20}
-            className="rounded-full"
-          />
-          <span>
-            {languages.find((lang) => lang.code === activeLocale)?.label}
-          </span>
-          <ChevronDown size={16} />
-        </button>
-        {dropdownVisible && (
-          <div className="absolute right-0 mt-2 bg-white text-gray-800 rounded-md shadow-lg w-36">
-            {languages.map(({ code, label, flag }) => (
-              <button
-                key={code}
-                onClick={() => handleLanguageChange(code)}
-                className="flex items-center space-x-2 w-full px-4 py-2 hover:bg-gray-100"
-              >
-                <Image
-                  src={flag}
-                  alt={code}
-                  width={20}
-                  height={20}
-                  className="rounded-full"
-                />
-                <span>{label}</span>
-              </button>
-            ))}
-          </div>
-        )}
+  {/* Navigation Links */}
+  <ul
+    className={`${
+      menuOpen ? "block" : "hidden"
+    } lg:flex lg:space-x-6 bg-gray-800 lg:bg-transparent lg:flex-row lg:static absolute top-full left-0 w-full lg:w-auto p-4 lg:p-0`}
+  >
+    <li className="text-center">
+      <Link
+        href={`/?lang=${activeLocale}#about`}
+        className="hover:text-yellow-400 block"
+      >
+        {t("about.title")}
+      </Link>
+    </li>
+    <li className="text-center">
+      <Link
+        href={`/?lang=${activeLocale}#services`}
+        className="hover:text-yellow-400 block"
+      >
+        {t("services.title")}
+      </Link>
+    </li>
+    <li className="text-center">
+      <Link
+        href={`/?lang=${activeLocale}#fleet`}
+        className="hover:text-yellow-400 block"
+      >
+        {t("fleet.title")}
+      </Link>
+    </li>
+    <li className="text-center">
+      <Link
+        href={`/?lang=${activeLocale}#contact`}
+        className="hover:text-yellow-400 block"
+      >
+        {t("contact.title")}
+      </Link>
+    </li>
+  </ul>
+
+  {/* Language Selector */}
+  <div className="relative">
+    <button
+      onClick={() => setDropdownVisible(!dropdownVisible)}
+      className="flex items-center space-x-2 bg-gray-700 px-4 py-2 rounded-md hover:bg-gray-600"
+    >
+      <Image
+        src={
+          languages.find((lang) => lang.code === activeLocale)?.flag || ""
+        }
+        alt={activeLocale}
+        width={20}
+        height={20}
+        className="rounded-full"
+      />
+      <span>
+        {languages.find((lang) => lang.code === activeLocale)?.label}
+      </span>
+      <ChevronDown size={16} />
+    </button>
+    {dropdownVisible && (
+      <div className="absolute right-0 mt-2 bg-white text-gray-800 rounded-md shadow-lg w-36">
+        {languages.map(({ code, label, flag }) => (
+          <button
+            key={code}
+            onClick={() => handleLanguageChange(code)}
+            className="flex items-center space-x-2 w-full px-4 py-2 hover:bg-gray-100"
+          >
+            <Image
+              src={flag}
+              alt={code}
+              width={20}
+              height={20}
+              className="rounded-full"
+            />
+            <span>{label}</span>
+          </button>
+        ))}
       </div>
-    </nav>
+    )}
+  </div>
+</nav>
+
   );
 }
--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -46,7 +46,7 @@ export default function Header() {
   return (
     <section
       id="uvod"
-      className="h-screen bg-white bg-cover bg-center relative"
+      className="h-[89vh] bg-white bg-cover bg-center relative"
     >
       {/* Background image */}
       <div
@@ -58,28 +58,37 @@ export default function Header() {
         }}
       ></div>
 
-      {/* Content */}
-      <div className="absolute left-1/2 top-2/3 transform -translate-x-1/2 -translate-y-1/2 w-5/6 sm:w-3/4 lg:w-2/3 bg-white bg-opacity-60 backdrop-blur-lg rounded-lg shadow-lg text-center p-8">
-        <h1 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold tracking-wide text-black mb-6">
-          {t("home.subtitle")}
-        </h1>
-        <p className="text-base sm:text-lg lg:text-xl text-gray-700 leading-relaxed">
-          {t("home.description")}
-        </p>
-      </div>
+{/* Content */}
+<div className="absolute left-1/2 top-[70%] transform -translate-x-1/2 -translate-y-1/2 w-5/6 sm:w-3/4 lg:w-2/3 bg-white bg-opacity-60 backdrop-blur-lg rounded-[10px] shadow-xl text-center p-6 border border-white border-opacity-30">
+  <h1 className="text-3xl sm:text-4xl lg:text-5xl font-black tracking-wider text-gray-900 mb-3 drop-shadow-md">
+    {t("home.subtitle")}
+  </h1>
+  <div className="my-2 w-4/5 mx-auto border-t-2 border-gray-500"></div>
+  <p className="text-sm sm:text-base lg:text-lg text-gray-800 leading-relaxed tracking-wide drop-shadow-sm">
+    {t("home.description")}
+  </p>
+</div>
 
-      {/* Dots for manual navigation */}
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex space-x-2">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            onClick={() => handleDotClick(index)}
-            className={`w-3 h-3 rounded-full transition-colors duration-300 ${
-              index === currentImage ? "bg-blue-500" : "bg-gray-300"
-            }`}
-          ></button>
-        ))}
-      </div>
+{/* Dots for manual navigation */}
+<div
+  className="absolute left-1/2 flex space-x-2 transform -translate-x-1/2"
+  style={{
+    bottom: '2.5%', // Positioning dots dynamically below the container
+  }}
+>
+  {images.map((_, index) => (
+    <button
+      key={index}
+      onClick={() => handleDotClick(index)}
+      className={`w-3 h-3 rounded-full transition-colors duration-300 ${
+        index === currentImage ? "bg-blue-500" : "bg-gray-300"
+      }`}
+    ></button>
+  ))}
+</div>
+
+
+
     </section>
   );
 }
