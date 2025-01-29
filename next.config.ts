a47413a281153956d1ad9a3aa0719db519c72607
Lukas Kouril <kouril.lukas@gmail.com>
2025-01-29 16:18:39 +0700

    build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
