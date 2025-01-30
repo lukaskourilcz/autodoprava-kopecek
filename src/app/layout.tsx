a3993a6b306a0d29cd6099a224525c48ea0d274b
Lukas Kouril <kouril.lukas@gmail.com>
2025-01-30 19:50:59 +0700

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,14 @@ import "../styles/globals.css";
 import Navbar from "./components/Navbar";
 
 export const metadata = {
-  title: "Autodoprava Kopeček",
+  title: "Autodoprava Kopeček | Váš spolehlivý dopravní partner.",
   description:
     "Jsme tu pro vás, ať už potřebujete zajistit pohodlnou přepravu osob nebo bezpečný transport nákladu.",
+    icons: {
+      icon: "/favicon.ico", 
+      shortcut: "/favicon.ico",
+      apple: "/favicon.png",
+    },
 };
 
 export default function RootLayout({
