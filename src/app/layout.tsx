b4a42b57da44b66e2cf1a02772840f7ecd0eacbf
Lukas Kouril <kouril.lukas@gmail.com>
2025-01-30 20:04:38 +0700

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from "react";
 import "../styles/globals.css";
 import Navbar from "./components/Navbar";
+import Head from "next/head";
 
 export const metadata = {
   title: "Autodoprava Kopeček | Váš spolehlivý dopravní partner",
@@ -20,6 +21,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="cs">
+      <Head>
+      <meta property="og:title" content="Autodoprava Kopeček | Váš spolehlivý dopravní partner" />
+        <meta property="og:description" content="Moderní a spolehlivá autobusová doprava v Hodoníně. Zajišťujeme komfortní přepravu pro skupiny i jednotlivce." />
+        <meta property="og:image" content="https://www.autobusyhodonin.cz/_next/image?url=%2Fpics%2Ftourismo.png&w=750&q=75" />
+        <meta property="og:url" content="https://www.autobusyhodonin.cz" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="cs_CZ" />
+      </Head>
       <body>
         <Suspense fallback={<p>Loading navbar...</p>}>
           <Navbar />
